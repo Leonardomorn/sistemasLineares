ca5f29af20379ada3acd8aa6172c68f68804c490
camaleoao <camargo.leonardo1@gmail.com>
2022-11-18 19:36:31 -0300

    eliminacao gauss arrumada (timestamp), gausseildel feita(falta norma) e refinamento começada)

--- Metodos.c
+++ Metodos.c
@@ -96,9 +96,21 @@ void retrosubs(SistLinear_t *SL, real_t *vetorSolucao)
   \param SL Ponteiro para o sistema linear
   \param x Solução do sistema linear
 */
-real_t normaL2Residuo(SistLinear_t *SL, real_t *x)
+real_t normaL2Residuo(SistLinear_t *SL, real_t *residuo)
 {
-  
+ int tam = SL->n;
+ real_t *auxResiduo = malloc (SL->n * sizeof(real_t));
+
+ real_t normaL2 = 0.0;
+ for (int i = 0; i < tam; i++)
+ {
+  auxResiduo[i] = residuo[i] * residuo[i];
+ }
+ 
+ normaL2 = ABS(somaKahan(auxResiduo, tam));
+
+ free(auxResiduo);
+ return normaL2; 
 }
 
 
@@ -115,7 +127,70 @@ real_t normaL2Residuo(SistLinear_t *SL, real_t *x)
   */
 int gaussSeidel (SistLinear_t *SL, real_t *x, real_t erro, double *tTotal)
 {
+  int tam = SL->n;
+  int iteracoes = 0;
+  real_t *xAux;
+  zeraVetorSolucao(SL, x);
+  xAux = (real_t*) malloc(tam * sizeof(real_t));
+
+
+  for (iteracoes = 0; iteracoes < MAXIT; iteracoes++)
+  {
+    atribuiAuxSolucao(SL, xAux, x);
   
+    for (int i = 0; i<tam; i++)
+    {
+      x[i] = SL->b[i];
+      for (int j = i+1; j < tam; j++)
+      {
+        x[i] = x[i] - SL->A[i][j] * x[j];
+      }
+      for (int j = i-1; j>= 0; j--)
+      {
+        x[i] = x[i] - SL->A[i][j] * x[j];
+      }
+      x[i] = x[i] / SL->A[i][i];
+    }
+
+
+    if (erroMaximo(SL, xAux, x) <= erro)
+    {
+      return iteracoes;
+      free(xAux);
+    }
+  }
+  free(xAux);
+  return iteracoes;
+}
+
+real_t erroMaximo(SistLinear_t* SL,real_t *xAux, real_t *x)
+{
+  real_t maxErro = 0;
+  for (int i = 0; i < SL->n; i++)
+  {
+    if (maxErro < (abs(xAux[i] - x[i])))
+      maxErro = abs(xAux[i] - x[i]);
+  }
+  return maxErro;
+}
+
+int atribuiAuxSolucao(SistLinear_t *SL,real_t *xAux, real_t *x)
+{
+  for (int i = 0; i < SL->n; i++)
+  {
+    xAux[i] = x[i];
+  }
+
+  return 1;
+  
+}
+int zeraVetorSolucao(SistLinear_t *SL, real_t *x)
+{
+  int tam = SL->n;
+  for (int i = 0; i<tam ; i++)
+  {
+    x[i] = 0;
+  }  
 }
 
 
@@ -130,9 +205,80 @@ int gaussSeidel (SistLinear_t *SL, real_t *x, real_t erro, double *tTotal)
   \return código de erro. Um nr positivo indica sucesso e o nr
           de iterações realizadas. Um nr. negativo indica um erro.
   */
-int refinamento (SistLinear_t *SL, real_t *x, real_t erro, double *tTotal)
+int refinamento (SistLinear_t *SL, real_t *vetorSolucao, real_t erro, double *tTotal)
 {
+*tTotal = timestamp();
+real_t *residuo = malloc (SL->n * sizeof (real_t));
+real_t variavelDescartavel; // apenas para poder chamar a eliminacao de gauss
+real_t normaL2;
+int tam = SL->n;
+int iteracoes = 0;
+//passo 1 
+//obter solucao inicial
+eliminacaoGauss(SL, vetorSolucao, &variavelDescartavel);
+
+for (int iteracoes = 0; iteracoes < tam; iteracoes++)
+{
+  //passo 2
+  // Calcular o resíduo r = b - Ax(i) e testar critério de parada (a)
+  calculaEAtribuiResiduo(SL, vetorSolucao, residuo);
+  normaL2 = normaL2Residuo(SL, residuo); 
+  if (normaL2 < ERRO)
+  {
+    free(residuo);
+    return iteracoes;
+  }
+  
+  
+}
+
+//passo 3
+//
+
+
+//passo 4
+//
+
+//passo 5
+//
+
+free(residuo);
+}
+
+//funcao que calcula o residuo e atribui na variável residuo
+void calculaEAtribuiResiduo(SistLinear_t* SL, real_t *vetorSolucao, real_t *residuo)
+{
+  int tam = SL->n; 
+  real_t *resAux = malloc(tam * sizeof(real_t)); 
+
+  for (int i = 0; i < tam; i++)
+  {
+    residuo[i] = 0.0;
+    for (int j = i; j < tam; j++)
+    {
+      resAux[j] = SL->A[i][j] * vetorSolucao[j];
+    }
+    residuo[i] = somaKahan(resAux, tam); //soma do lado esquerdo do Sislin (Ax)
+    residuo[i] = SL->b[i] - residuo[i]; // r = b - Ax
+
+  }
+  
+  free (resAux);
 
 }
 
+//algoritmo de soma para evitar muitos cancelamentos subtrativos
+real_t somaKahan( real_t *dados, int tam )
+{
+    real_t soma = 0.0; // Prepara o acumulador
+    real_t compensador = 0.0;   // compensador para a perda de bits de baixa ordem
 
+    for(int i = 0; i < tam; i++)
+    {
+        real_t y = dados[i] - compensador;
+        real_t t = soma + y;
+        compensador = (t - soma) - y;
+        soma = t;
+    }
+    return soma;
+}
--- Metodos.h
+++ Metodos.h
@@ -6,7 +6,7 @@
 #define ERRO 1.0e-10  // Tolerância para critérios de parada em métodos iterativos
 
 // Calcula a normaL2 do resíduo
-real_t normaL2Residuo(SistLinear_t *SL, real_t *x);
+real_t normaL2Residuo(SistLinear_t *SL, real_t *residuo);
 
 // Método da Eliminação de Gauss
 int eliminacaoGauss (SistLinear_t *SL, real_t *vetorSolucao, double *tTotal);
@@ -19,6 +19,12 @@ int refinamento (SistLinear_t *SL, real_t *x, real_t erro, double *tTotal);
 
 // Método de Gauss-Seidel
 int gaussSeidel (SistLinear_t *SL, real_t *x, real_t erro, double *tTotal);
+int zeraVetorSolucao(SistLinear_t *SL, real_t *x);
+int atribuiAuxSolucao(SistLinear_t *SL,real_t *xAux, real_t *x);
+real_t erroMaximo(SistLinear_t* SL,real_t *xAux, real_t *x);
+
+
+
 
 #endif // __METODOS_H__
 
